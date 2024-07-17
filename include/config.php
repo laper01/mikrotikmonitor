aff68fedbdb10332960e645d9681321b16e7fb16
laper01 <yusufyahya0107@gmail.com>
2024-07-17 16:21:50 +0800

    edit-tempalte

---
+++ include/config.php
@@ -0,0 +1,7 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<ummat','mikhmon>|>q5aZmaOTeJih');
+
+$data['new-8411'] = array ('1'=>'new-8411!','new-8411@|@','new-8411#|#','new-8411%','new-8411^','new-8411&Rp','new-8411*10','new-8411(1','new-8411)','new-8411=10','new-8411@!@disable');
+$data['new-9225'] = array ('1'=>'new-9225!','new-9225@|@','new-9225#|#','new-9225%','new-9225^','new-9225&Rp','new-9225*10','new-9225(1','new-9225)','new-9225=10','new-9225@!@disable');
+$data['new-5146'] = array ('1'=>'new-5146!','new-5146@|@','new-5146#|#','new-5146%','new-5146^','new-5146&Rp','new-5146*10','new-5146(1','new-5146)','new-5146=10','new-5146@!@disable');
